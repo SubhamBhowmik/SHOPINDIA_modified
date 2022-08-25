96bdc56156b927f333856ea9582159f3157f096a
Subham <czsubham@gmail.com>
2022-08-25 10:31:07 +0530

    final touchup

---
+++ server/constants/product.js
@@ -0,0 +1,151 @@
+export const products = [
+    { 
+        id: 'product1',
+        url:'https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+        detailUrl:'https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60', 
+        title: {
+            shortTitle: 'Home & Kitchen',
+            longTitle: 'Pigeon FAVOURITE Electric Kettle  (1.5 L, Silver, Black)'
+        }, 
+        price: {
+            mrp: 1195,
+            cost: 625,
+            discount: '47%'
+        },
+        quantity: 1,
+        description: 'This electric kettle from Pigeon will soon become a travelers best friend, a hostelite saviour and an answer to all the midnight cravings. With this handy appliance, you can boil water and use it to make instant noodles, packet soup, coffee and green tea.',
+        discount: 'Extra 10% Off', 
+        tagline: 'Deal of the day' 
+    },
+    { 
+        id: 'product2',
+        url: 'https://images.unsplash.com/photo-1543512214-318c7553f230?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+
+        detailUrl:'https://images.unsplash.com/photo-1543512214-318c7553f230?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+        title: {
+            shortTitle: 'Sandwich Makers',
+            longTitle: 'Flipkart SmartBuy Sandwich 01 Grill  (Black)'
+        },
+        price: {
+            mrp: 1499,
+            cost: 899,
+            discount: '40%'
+        },
+        quantity: 1,
+        description: 'This non-stick sandwich toaster .easy to use and very handy. Directly hold over flame to make tasty toasts and toasted sandwiches. Specially designed by keeping your needs in mind, the sandwich maker makes whatever youre doing simpler, smarter and better',
+        discount: 'From 99+5% Off', 
+        tagline: 'Pestige, Nova & more' 
+    },
+    { 
+        id: 'product3',
+        url:'https://images.unsplash.com/photo-1543512214-318c7553f230?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+
+        detailUrl:'https://images.unsplash.com/photo-1543512214-318c7553f230?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+        title: {
+            shortTitle: 'Fitness Gear',
+            longTitle: 'AJRO DEAL New Adjustable Single Resistance Tube (Multicolor) Resistance Tube  (Multicolor)'
+        }, 
+        price: {
+            mrp: 499,
+            cost: 166,
+            discount: '66%'
+        },
+        quantity: 1,
+        description: 'This unique product can tone your back muscles, reduce belly fat, improve blood circulation and also improves your body posture. It increases the stamina, energy and vitality of the body. The elastic resistance of the rubber training rope can be used to train and exercise in whichever way you want, according to your physical needs.',
+        discount: 'Upto 70% Off', 
+        tagline: 'Deal of the Day' 
+    },
+    { 
+        id: 'product4',
+        url:'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+
+        detailUrl: 'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+
+        title: {
+            shortTitle: 'Smart Watches',
+            longTitle: 'Molife Sense 500 Smartwatch  (Black Strap, Freesize)',
+        }, 
+        price: {
+            mrp: 6999,
+            cost: 4049,
+            discount: '42%'
+        },
+        quantity: 1,
+        description: 'The Molife Sense 500, a brilliant smartwatch with a beautiful large display. Say hello to the infinity 1.7-inch display with 2.5D curved edges. Thanks to seamless Bluetooth 5.0 connectivity, you wont have to keep waiting. Bring a change to your outfit every day with changeable straps. A splash of color every day keeps the boredom away.',
+        discount: 'Grab Now', 
+        tagline: 'Best Seller' 
+    },
+    { 
+        id: 'product5',
+        url:'https://images.unsplash.com/photo-1583394838336-acd977736f90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+        detailUrl:'https://images.unsplash.com/photo-1583394838336-acd977736f90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+
+        title: {
+            shortTitle: 'Trimmers, Dryers & more',
+            longTitle: 'Nova Professional NHP 8220 Hair Dryer  (1800 W, Multicolor)'
+        }, 
+        price: {
+            mrp: 1899,
+            cost: 1124,
+            discount: '40%'
+        },
+        quantity: 1,
+        description: '',
+        discount: 'From ₹499', 
+        tagline: 'Kubra, Nova & more' 
+    },
+    { 
+        id: 'product6',
+        url: 'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        detailUrl: 'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        title: {
+            shortTitle: 'Table Fans',
+            longTitle: 'Portable 300 mm Ultra High Speed 3 Blade Table Fan  (Black, Pack of 1)'
+        }, 
+        price: {
+            mrp: 2250,
+            cost: 1199,
+            discount: '46%'
+        },
+        quantity: 1,
+        description: 'Table Fan. Perfect size fan for use on a table, desk or in an RV. Whisper quiet, powerful airflow and reliable operation in a compact 6" size. Two adjustable speeds to customize airflow: high or low settings. Tough break-resistant ABS plastic blades. ',
+        discount: 'Minimum 40% Off', 
+        tagline: 'Top Selling' 
+    },
+    { 
+        id: 'product7',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4whU46unLZdYE4jMG1XuToK_xQ1HKY06ikw&usqp=CAU',
+        detailUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4whU46unLZdYE4jMG1XuToK_xQ1HKY06ikw&usqp=CAU', 
+        title: {
+            shortTitle: 'MI Note 8',
+            longTitle: 'MI Note 8,4GB 128 GB, Midnight (Black) '
+        }, 
+        price: {
+            mrp: 13999,
+            cost: 9999,
+            discount: '12%'
+        },
+        quantity: 1,
+        description: 'Let music brighten up your mood anytime, anywhere with the boAt 235v2 Fast Charging Bluetooth Headset. This Bluetooth headset features a Call Vibration Alert, a Fast Charging Technology, and Easy Access Controls to listen to and manage your favorite music with ease.',
+        discount: 'Minimum 50% Off', 
+        tagline: 'Grab Now!' 
+    },
+    { 
+        id: 'product8',
+        url:'https://www.reliancedigital.in/medias/Apple-MLPF3HN-A-Smart-Phones-491997699-i-3-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNTM3NTF8aW1hZ2UvanBlZ3xpbWFnZXMvaGJjL2gyYS85Nzc2MDM5MTAwNDQ2LmpwZ3wxMTQ1ODYwOWU4OTk3ZmM4Y2ZjYzQ4MWUwNTIyMTU2NzVkYTdlOWE4ZWMxN2RiYzc1MTJmNzZkNzRhYzhlZDgx',
+        detailUrl:'https://www.reliancedigital.in/medias/Apple-MLPF3HN-A-Smart-Phones-491997699-i-3-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNTM3NTF8aW1hZ2UvanBlZ3xpbWFnZXMvaGJjL2gyYS85Nzc2MDM5MTAwNDQ2LmpwZ3wxMTQ1ODYwOWU4OTk3ZmM4Y2ZjYzQ4MWUwNTIyMTU2NzVkYTdlOWE4ZWMxN2RiYzc1MTJmNzZkNzRhYzhlZDgx',
+        title: {
+            shortTitle: 'Apple Iphone 13',
+            longTitle: '128 GB, Midnight (Black) '
+        }, 
+        price: {
+            mrp: 72990.00,
+            cost: 60000,
+            discount: '11%'
+        },
+        quantity: 1,
+        description: 'Let music brighten up your mood anytime, anywhere with the boAt 235v2 Fast Charging Bluetooth Headset. This Bluetooth headset features a Call Vibration Alert, a Fast Charging Technology, and Easy Access Controls to listen to and manage your favorite music with ease.',
+        discount: 'Minimum 50% Off', 
+        tagline: 'Grab Now!' 
+    }
+];
